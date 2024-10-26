d9d9492bbb1a44c29962f3add3cf045f084c4dae
Analia <test@gmail.com>
2024-10-26 11:19:41 -0300

    updated WebSecurityConfig a with userDetailsService to grant access control to users in Memory

--- src/main/java/com/Alkemy/JavaMod21/security/config/WebSecurityConfig.java
+++ src/main/java/com/Alkemy/JavaMod21/security/config/WebSecurityConfig.java
@@ -1,27 +1,70 @@
 package com.Alkemy.JavaMod21.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
-    // testLogin: user & Using generated security password
-    // access: /admin only aauthenticated
+
+    // access: /admin only authenticated CON usuarios es memoria
+
+
     @Bean
     SecurityFilterChain web(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/authbasic/publico/**").permitAll()
-                        .requestMatchers("/authbasic/admin/**").authenticated()
+                        .requestMatchers("/authbasic/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
                 );
     http
         .formLogin(withDefaults()); // (1)
+    /* .formLogin(form -> form
+            .loginPage("/login")  // Página de login personalizada
+            .defaultSuccessUrl("/dashboard", true)  // Redirección al éxito
+            .failureUrl("/login?error=true")  // Redirección al fallo
+            .permitAll()*/
+    http
+        .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .permitAll()
+        );
+    http
+        .exceptionHandling(exception -> exception
+                .accessDeniedPage("/access-denied")
+        );
+
     http
         .httpBasic(withDefaults()); // (1)
     return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
--- src/main/java/com/Alkemy/JavaMod21/security/controller/BasicSecurityController2.java
+++ src/main/java/com/Alkemy/JavaMod21/security/controller/BasicSecurityController2.java
@@ -4,6 +4,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,6 +46,23 @@ public class BasicSecurityController2 {
 
         Map<String, String> mensaje = new HashMap<>();
         mensaje.put("contenido", "Hola Admin");
+        mensaje.put("user",auth.getPrincipal().toString());
+
+        return ResponseEntity.ok(mensaje);
+    }
+
+    @GetMapping("/adminPreAuthorize")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> getMensajeAdminPreAutorizado() {
+
+        var auth =  SecurityContextHolder.getContext().getAuthentication();
+
+        logger.info("Datos del Usuario: {}", auth.getPrincipal());
+        logger.info("Datos de los Permisos {}", auth.getAuthorities());
+        logger.info("Esta autenticado {}", auth.isAuthenticated());
+
+        Map<String, String> mensaje = new HashMap<>();
+        mensaje.put("contenido", "Hola Admin preautorizado");
         mensaje.put("user",auth.getPrincipal().toString());
 
         return ResponseEntity.ok(mensaje);
